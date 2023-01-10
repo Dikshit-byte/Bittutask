9fdf3788b7c9dcb6acaa3d329b386a1e0cb7e37d
Dikshit Singh <eminemd786@gmail.com>
2023-01-10 18:50:50 +0530

    use map function for iteration

--- github/index.js
+++ github/index.js
@@ -1,9 +1,8 @@
 import fetch from "node-fetch";
 
 function feedingData(data){
-    // console.log(`Repository Name : ${data[0].name}\nRepository URL : ${data[0].html_url}\nRepository Stars : ${data[0].stargazers_count}\nRepository Description : ${data[0].description}\nForks Count : ${data[0].forks_count}\nOpen Issue : ${data[0].open_issues_count}`) 
-    data.map((id,data1)=>{
-        console.log(`Repository ID: ${id}\nRepository Name : ${data1.name}\nRepository URL : ${data1.html_url}\nRepository Stars : ${data1.stargazers_count}\nRepository Description : ${data1.description}\nForks Count : ${data1.forks_count}\nOpen Issue : ${data1.open_issues_count}`)
+    data.map((data1)=>{
+        console.log(`Repository Name : ${data1.name}\nRepository URL : ${data1.html_url}\nRepository Stars : ${data1.stargazers_count}\nRepository Description : ${data1.description}\nForks Count : ${data1.forks_count}\nOpen Issue : ${data1.open_issues_count}\n\n`)
     })
 }
 async function getResponse(feedingData){ 
