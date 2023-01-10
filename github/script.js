70974dd58de8720704cf67a5c4e87b4e8509382c
Dikshit Singh <eminemd786@gmail.com>
2023-01-10 21:00:20 +0530

    add scirpt

--- github/script.js
+++ github/script.js
@@ -1,27 +1,35 @@
-import fetch from "node-fetch";
-
-function feedingData(data){
+const fetch = require("node-fetch");
+async function getResponse(feedingData) {
+    const data = await fetch("https://api.github.com/users/dikshit-byte/repos");
+    try {
+        let response = await data.json();
+        feedingData(response);
+    } catch (err) {
+        console.log("Error : ", err);
+    }
+}
+function feedingData(data) {
     data.map((data1)=>{
         console.log(`Repository Name : ${data1.name}\nRepository URL : ${data1.html_url}\nRepository Stars : ${data1.stargazers_count}\nRepository Description : ${data1.description}\nForks Count : ${data1.forks_count}\nOpen Issue : ${data1.open_issues_count}\n\n`)
+        getCommit(data1.name);
     })
-}
-async function getResponse(feedingData){ 
-    const data = await fetch("https://api.github.com/users/freeCodeCamp/repos");
-    try{
-    let response = await data.json();
-    feedingData(response);
-    }catch(err){
-        console.log("Error : ",err);    
-    }
-}
-async function getCommit(repo_name){ 
-    const data = await fetch(`https://api.github.com/repos/freeCodeCamp/${repo_name}/commits`);
-    try{
-    let response = await data.json();
-    feedingData(response);
-    }catch(err){
-        console.log("Error : ",err);    
-    }
 }
 
 getResponse(feedingData);
+
+async function getCommit(repo_name) {
+    const commitData = await fetch(`https://api.github.com/repos/dikshit-byte/${repo_name}/commits`);
+    try {
+        let response = await commitData.json();
+        feedCommit(response);
+    } catch (err) {
+        console.log("Error : ", err);
+    }
+}
+
+function feedCommit(repo_commit) {
+    console.log("Commit is : " + repo_commit[0].sha);
+    repo_commit.map((data)=>{
+        console.log(`Repository last_commit_sha : ${data.sha}\nRepository last_commit_message : ${data.message}`)
+    })
+}
